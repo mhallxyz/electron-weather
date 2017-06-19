9c9bfbdd58a763fecc9d43bc61bee50088292f0d
Mathew Hall <mhmathewhall@gmail.com>
2017-06-19 16:54:58 +0100

    Chart styling changed to equal heights

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,15 +8,16 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const tempsC = temps.map(entry => entry - 273.15);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return(
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color="blue"/></td>
-        <td><Chart data={pressures} color="grey"/></td>
-        <td><Chart data={humidities} color="red"/></td>
+        <td><Chart data={tempsC} color="blue" units="C"/></td>
+        <td><Chart data={pressures} color="grey" units="hPa"/></td>
+        <td><Chart data={humidities} color="red" units="%"/></td>
       </tr>
     );
   }
@@ -27,9 +28,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
