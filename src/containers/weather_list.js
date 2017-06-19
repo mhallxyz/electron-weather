0150bb3b31f03924b9e06ead872a06fbf5e2f451
Mathew Hall <mhmathewhall@gmail.com>
2017-06-19 16:21:18 +0100

    Charts display when search is submitted

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Sparklines, SparklinesLine } from "react-sparklines";
+import Chart from "../components/chart";
+
 
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps)
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return(
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="blue"/></td>
+        <td><Chart data={pressures} color="grey"/></td>
+        <td><Chart data={humidities} color="red"/></td>
       </tr>
     );
   }
