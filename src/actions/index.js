9fd5abd219b509560bbe8ac04b5b2cc72b2651d3
Mathew Hall <mhmathewhall@gmail.com>
2017-06-19 19:23:36 +0100

    HTTP re-added, Google Maps removed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const API_KEY = "e7e046fc9ad9390c6ead828430787972";
-const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER"
 
 export default function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city}`; // add ,uk to url to limit search to the uk
+  const url = `${ROOT_URL}&q=${city}`;// add ,uk to url to limit search to the uk
   const request = axios.get(url);
 
 
