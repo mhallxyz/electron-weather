6703ca6063b12c79799d0eea1b365ac4eb982d2f
Mathew Hall <mhmathewhall@gmail.com>
2017-06-19 19:05:36 +0100

    HTTP removed to avoid non-secure script error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "e7e046fc9ad9390c6ead828430787972";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER"
 
