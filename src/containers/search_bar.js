861612ae6e3f7c7b92e066808e9131cfe601c780
Mathew Hall <mhall.js@gmail.com>
2018-09-18 14:31:21 +0100

    Electron added

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -41,7 +41,7 @@ class SearchBar extends Component {
           onChange={this.onInputChange}
          />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </span>
       </form>
     );
